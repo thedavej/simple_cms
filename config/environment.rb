db876117cd8900a284aadac4bf8bfa7ddf378b49
Dave Johnson <thedavej@me.com>
2014-07-24 03:10:51 -0700

    Added custom time format to environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Time::DATE_FORMATS[:nice] = "%b %d, %Y at %l:%M %p" 
+Time::DATE_FORMATS[:abbrev] = "%m/%d/%y, %H:%M:%S" 
